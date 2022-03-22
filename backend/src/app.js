ecc4bb192ad161c46a0f37fc19edf2c79695d917
eethan1 <call132call132@gmail.com>
2022-03-22 17:47:32 +0800

    add frontend static

--- backend/src/app.js
+++ backend/src/app.js
@@ -1,8 +1,16 @@
-const app = require('express')()
-const PORT = process.env.PORT || 3301
-
-app.get('/', (req, res) => {
+const express = require('express')
+const path = require('path')
+const app = express()
+const PORT = process.env.PORT || 8000
+console.log(__dirname)
+const frontStatic = path.join(__dirname, '../../frontend/build')
+app.use(express.static(frontStatic))
+app.get('/hello', (req, res) => {
   res.send('HelloworlD')
+})
+
+app.get('*', (req, res) => {
+  res.sendFile(frontStatic + '/index.html')
 })
 
 app.listen(PORT, () => {
